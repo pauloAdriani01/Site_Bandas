ff1c76bf238ffdb1e3e5381368380a1f3f635c42
Paulo Henrique S. Adriani <paulohadriani2007@gmail.com>
2025-05-27 21:00:06 -0300

    Commit 11 - Organização
    - Finalização do JS nas páginas principais
    - União de todos os CSS em um único documento
    - Criação da página de login

---
+++ script/script.js
@@ -0,0 +1,48 @@
+//Funcionamento do Botão de Pesquisa (Todas as Páginas)
+
+const obj_botao_pesquisa = document.querySelector("#botao_pesquisa");
+
+obj_botao_pesquisa.addEventListener("click", funPesquisa);
+
+function funPesquisa() {
+    alert("Pesquisando...");
+    location.reload();
+}
+
+// Funcionamento do Botão "Carregar Mais" (Notícias, Posts e Bandas)
+
+const pag_ativa = window.location.pathname.toLowerCase(); //Verifica a página ativa 
+
+if (pag_ativa.includes("noticias") || pag_ativa.includes("psots") || pag_ativa.includes("bandas")) {
+
+    const obj_botao_mais = document.querySelector("#botao_mais");
+
+    obj_botao_mais.addEventListener("click", funCarregarMais);
+
+    function funCarregarMais() {
+    
+        if (pag_ativa.includes("noticias")) {
+            alert("Carregando mais notícias...");
+
+        } else if (pag_ativa.includes("posts")) {
+            alert("Carregando mais posts...");
+
+        } else if (pag_ativa.includes("bandas")) {
+            alert("Carregando mais bandas...");
+
+        }
+    }
+
+}
+
+//Funcionamento dos Links para Redes Sociais (Todas as Páginas)
+
+const array_rede_click = document.querySelectorAll(".rede_click"); //Pega todos os meus links na classe
+
+array_rede_click.forEach(obj_rede_click => { //Aplica o "eventListener" em cada link
+    obj_rede_click.addEventListener("click", funRede);
+});
+
+function funRede() {
+    alert("Redirecionando para a rede social...");
+}
