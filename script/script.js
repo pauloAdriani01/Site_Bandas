549b02b10b3187d5b760f3b1421e7075d50ed3d7
Paulo Henrique S. Adriani <paulohadriani2007@gmail.com>
2025-06-02 21:54:37 -0300

    Commit 13 - Página de Login
    - Finalização completa da página de login

--- script/script.js
+++ script/script.js
@@ -1,27 +1,26 @@
+const pag_ativa = window.location.pathname.toLowerCase(); //Leitura da página ativa
+
 //Funcionamento do Botão de Pesquisa (Index, Notícias, Posts e Bandas)
 
-const obj_botao_pesquisa = document.querySelector("#botao_pesquisa");
+if (pag_ativa.includes("index") || pag_ativa.includes("noticias") || pag_ativa.includes("posts") || pag_ativa.includes("bandas")) {
+    const obj_botao_pesquisa = document.querySelector("#botao_pesquisa");
 
-obj_botao_pesquisa.addEventListener("click", funPesquisa);
+    obj_botao_pesquisa.addEventListener("click", funPesquisa);
 
-function funPesquisa() {
-        alert("Pesquisando...");
-        location.reload();
+    function funPesquisa() {
+            alert("Pesquisando...");
+            location.reload();
+    }
 }
-
 
 // Funcionamento do Botão "Carregar Mais" (Notícias, Posts e Bandas)
 
-const pag_ativa = window.location.pathname.toLowerCase(); //Leitura da página ativa
-
 if (pag_ativa.includes("noticias") || pag_ativa.includes("posts") || pag_ativa.includes("bandas")) {
-
     const obj_botao_mais = document.querySelector("#botao_mais");
 
     obj_botao_mais.addEventListener("click", funCarregarMais);
 
     function funCarregarMais() {
-    
         if (pag_ativa.includes("noticias")) {
             alert("Carregando mais notícias...");
 
@@ -46,4 +45,25 @@ array_rede_click.forEach(obj_rede_click => { //Aplica o "eventListener" em cada 
 
 function funRede() {
         alert("Redirecionando para a rede social...");
+}
+
+//Funcionamento do Botão de "Login" + Botão de "Criar Conta" (Login)
+
+if (pag_ativa.includes("login")) {
+    const obj_botao_login = document.querySelector("#botao_login");
+    const obj_botao_criarConta = document.querySelector("#botao_criarConta");
+
+    obj_botao_login.addEventListener("click", funLogin);
+    obj_botao_criarConta.addEventListener("click", funCriarConta);
+
+    function funLogin() {
+        alert("Realizando seu login...");
+        location.reload();
+    }
+
+    function funCriarConta() {
+        alert("Criando sua conta...");
+        location.reload();
+    }
+
 }
